b29c0d92bc4ac72536f20ab01e4a55fce49a2528
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-07 10:45:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0 - success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
