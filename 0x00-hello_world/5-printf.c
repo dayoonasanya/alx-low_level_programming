c7c0207fe3ae7d69ce6bf8a8477631c6dae24c6c
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-07 11:04:21 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -main function
+ *
+ * Return: always 0 - success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
