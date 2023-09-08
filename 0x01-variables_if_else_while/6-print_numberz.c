0ba95618b60ed1a4f73e3042d59cd8f225aa7f62
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-08 14:36:43 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
