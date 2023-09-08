b53bbd72c645b693eab50445acd163ab6eb1374b
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-08 14:26:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d", j);
+	}
+	putchar('\n');
+	return (0);
+}
