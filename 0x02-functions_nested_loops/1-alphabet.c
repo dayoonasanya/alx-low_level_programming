06bdf2f6a952e1e9942ded84bbfafb4e932e74d8
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-12 14:11:06 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase letters.
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+
+}
