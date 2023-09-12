ee64e303482f6f0d9a7fd051712b0a1ee2c0dc16
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-12 15:16:00 -0700

    There are only 3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_last_digit - prints last digit
+ *
+ * Return 0 if success.
+ */
+int print_last_digit(int x)
+{
+	int z;
+
+	z = (x % 10);
+	if (z < 0)
+	{
+		z = (-1 * z);
+	}
+	_putchar(z + '0');
+	return (z);
+}
