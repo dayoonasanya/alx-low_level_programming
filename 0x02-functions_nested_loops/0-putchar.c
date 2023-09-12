c504209cd373924a00d2a92e162a88c4aaf5691e
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-12 13:56:15 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
