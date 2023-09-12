d6f93aae3d66299996decf7b129e6fc8c428f75d
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-12 14:50:55 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabet letter
+ *
+ * Return: 0 if success
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
