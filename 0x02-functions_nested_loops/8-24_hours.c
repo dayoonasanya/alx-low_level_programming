a915bf86790a361c30cf3cc0c25977d4c0b674c9
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-12 15:30:20 -0700

    I'm federal agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *jack_bauer - prints every minute.
+ *
+ * starting from 00:00 to 23:59
+ *Return: 0 if success
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
