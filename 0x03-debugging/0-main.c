b2c7cc0e913b57082bfa912466e00188cb4e8486
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-13 14:23:55 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -function:tests function that prints
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
