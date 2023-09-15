7eb6cc224e3721d77f7a5b585762cc42c8f30ad1
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-14 17:20:13 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print No (s)
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int n = 38;
+for (n = 38; n < 48; n++)
+{
+_putchar(n);
+}
+_putchar(10);
+}
