786e49bacaf498221d411ba274c6c2b0b7fb1951
dayoonasanya <aonasanya@mail.dccc.edu>
2023-09-21 05:57:27 -0700

    Main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,4 +20,4 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif /MAIN_H/
+#endif /*MAIN_H*/
