07221b1f0ffbc1896e55e2186ff6a1ee606923de
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-02 14:33:50 -0700

    o infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - function: will add positive numbers.
+*@argc: No. of arguments.
+*@argv: array arguments.
+*Return: returns 0 or 1.
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
