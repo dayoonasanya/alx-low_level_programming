cb58fea0c4d83742ad766b92df86352ee2d37d3e
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-03 11:02:36 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* alloc_grid - function: returns a pointer.
+*@width: width of array.
+*@height: height of array.
+*Return: pointer to the array
+*/
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i = 0, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	array = (int **) malloc(sizeof(int *) * height);
+	if (array != NULL)
+	{
+		for (; i < height; i++)
+		{
+			array[i] = (int *) malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
+		}
+		return (array);
+	}
+	else
+	{
+		return (NULL);
+	}
+
