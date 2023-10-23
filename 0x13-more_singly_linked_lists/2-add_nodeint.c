f4681e9c7c220c4d77bd62006c9ad6e94dc9aba3
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-23 12:33:57 -0700

    Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint - addition
+ * @head: header
+ * @n: integer.
+ *
+ * Return: new node.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
