79b16c8226ae268b91018e59c2500b71fdd08ab8
dayoonasanya <aonasanya@mail.dccc.edu>
2023-10-30 10:34:36 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ *append_text_to_file - append.
+ *@filename: pointer.
+ *@text_content: content.
+ *
+ *Return: NULL
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
