2cf8f91be0cda7f9176988fa43937befa85f11eb
dayoonasanya <aonasanya@mail.dccc.edu>
2023-12-11 10:22:06 -0800

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+int div(int a, int b)
+{
+if (b == 0)
+return (0);
+else
+return (a / b);
+}
+
+int mod(int a, int b)
+{
+if (b == 0)
+return (0);
+else
+return (a % b);
+}
